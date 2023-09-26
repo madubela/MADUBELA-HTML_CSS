6dcddf2d8cb336fcdffbbf7a09d40e88046cba71
@LangaMadubela <madubelalanga30@gmail.com>
2023-09-26 12:08:59 +0200

    changes

--- Images/mane.js
+++ Images/mane.js
@@ -90,3 +90,24 @@ document.addEventListener("DOMContentLoaded", function() {
       });
   });
 });
+
+   // Function to scroll to the top of the page
+   function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Add smooth scrolling behavior
+    });
+}
+
+// Show the button when the user scrolls down 20px from the top of the document
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById('scrollUpButton').style.display = 'block';
+    } else {
+        document.getElementById('scrollUpButton').style.display = 'none';
+    }
+}
